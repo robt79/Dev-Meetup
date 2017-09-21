13a4db74e65946926f706cd9e88268ae78f1ac02
Robert Torres <robt792003@gmail.com>
2017-09-21 17:08:41 -0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info grey darken-4">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info grey darken-4">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 class="text-xs-center">
+        <v-progress-circular
+        indeterminate
+        class="primary--text"
+        :width="7"
+        :size="70"
+        v-if="loading"></v-progress-circular>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2" v-if="!loading">
+      <v-flex xs12>
+        <v-carousel style="cursor: pointer;">
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id"
+            @click="onLoadMeetup(meetup.id)">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap  class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+       <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+    <v-footer class="pa-3 info grey darken-4" style= "height: 50px">
+      <v-spacer></v-spacer>
+      <div>© {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    computed: {
+      meetups () {
+        return this.$store.getters.featuredMeetups
+      },
+      loading () {
+        return this.$store.getters.loading
+      }
+    },
+
+    methods: {
+      onLoadMeetup (id) {
+        this.$router.push('/meetups/' + id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
